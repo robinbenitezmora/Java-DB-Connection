c208872ccf140d513a0acb099419447d1d031686
robinbenitezmora <robinbenitez1990@gmail.com>
2023-08-22 22:28:55 -0500

    Add 'ConnectionDB.java' class

---
+++ src/com/robin/com/ConnectionDB.java
@@ -0,0 +1,23 @@
+package com.robin.com;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionDB {
+	public static void main (String[] args) {
+		
+		Connection connection = null;
+		try {
+			Class.forName("org.postgresql.Driver");
+			connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","root");
+			
+			if(connection != null){
+				System.out.println("Connection OK!");
+			}else {
+				System.out.println("Connection Failed!");
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+}
