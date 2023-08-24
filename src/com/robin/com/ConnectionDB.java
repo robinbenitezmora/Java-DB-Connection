bb0a90640ef1c12e833aa5113ebe564a6b7e9022
robinbenitezmora <robinbenitez1990@gmail.com>
2023-08-23 21:57:44 -0500

    Fix the connection with DB

--- src/com/robin/com/ConnectionDB.java
+++ src/com/robin/com/ConnectionDB.java
@@ -4,16 +4,16 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class ConnectionDB {
-	public static void main (String[] args) {
-		
+	public static void main(String[] args) {
+
 		Connection connection = null;
 		try {
 			Class.forName("org.postgresql.Driver");
-			connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","root");
-			
-			if(connection != null){
+			connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "peti2020#");
+
+			if (connection != null) {
 				System.out.println("Connection OK!");
-			}else {
+			} else {
 				System.out.println("Connection Failed!");
 			}
 		} catch (Exception e) {
