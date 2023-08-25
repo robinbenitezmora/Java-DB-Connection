2aa1f89e02a9f53d859b8d3e61cc06b5daae186e
robinbenitezmora <robinbenitez1990@gmail.com>
2023-08-24 22:20:34 -0500

    Update 'ConnectionDB.java' class

---
+++ src/com/robin/com/ConnectDB.java
@@ -0,0 +1,37 @@
+package com.robin.com;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectDB {
+
+	public static void main(String[] args) {
+		ConnectDB obj_DB_Connection = new ConnectDB();
+		System.out.println(obj_DB_Connection.get_connection());
+	}
+
+	public Connection get_connection() {
+
+		Connection connection = null;
+		String host = "localhost";
+		String port = "5432";
+		String db_name = "postgres";
+		String username = "postgres";
+		String password = "peti2020#";
+
+		try {
+			Class.forName("org.postgresql.Driver");
+			connection = DriverManager.getConnection("jdbc:postgresql://" + host + ":" + port + "/" + db_name,
+					username, password);
+
+			if (connection != null) {
+				System.out.println("Connection OK!");
+			} else {
+				System.out.println("Connection Failed!");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return connection;
+	}
+}
