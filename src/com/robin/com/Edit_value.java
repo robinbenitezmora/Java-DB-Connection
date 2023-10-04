16ed5a40074dcc480739725c30517723ec9ca2de
robinbenitezmora <robinbenitez1990@gmail.com>
2023-10-03 21:30:26 -0500

    Create 'Edit_value.java' file

---
+++ src/com/robin/com/Edit_value.java
@@ -0,0 +1,23 @@
+package com.robin.com;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class Edit_value {
+	public static void main(String[] args) {
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ConnectDB obj_ConnectDB = new ConnectDB();
+		connection = obj_ConnectDB.get_connection();
+
+		try {
+			String query = "update employee set name='Jhonson' where sl_no='3'";
+			statement = connection.prepareStatement(query);
+			statement.executeUpdate();
+			System.out.println("Update complete");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
